813c5f7b41152246fdc395b991ae74576b632884
filvlasov <fil_vlasov@mail.ru>
2018-10-08 10:26:34 +0300

    https://geekbrains.ru/certificates/505069.ru

---
+++ HomeWork8.java
@@ -0,0 +1,12 @@
+/**
+ * Java. Level 1. Lesson 8. Homework
+ *
+ * @author Philipp Vlasov
+ * @version dated Oct 05, 2018
+ */
+public class HomeWork8 {
+
+    public static void main(String[] args){
+        System.out.println("Сергей, спасибо за курс JAVA 1!!!");
+    }
+}
